0aac4bc3174c19ce8da9e01962077bc86856f1bc
Microsoft Learn Student <learn@contoso.com>
2020-04-09 03:35:51 -0300

    Changes

--- Infra/CrossCutting/Shared/Resources/Messages.cs
+++
@@ -1,7 +0,0 @@
-namespace Shared.Resources
-{
-    public static class Messages
-    {
-        public readonly static string FORGOT_PASSWORD_SUCCESS = "Send Email, Verify your email client.";
-    }
-}
